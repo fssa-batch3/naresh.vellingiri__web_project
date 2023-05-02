bb79b0499d64fb8b92d8f13a851afcf48ba7e718
naresh031020000 <116252192+naresh031020000@users.noreply.github.com>
2023-05-02 14:13:24 +0530

    feat: main_update

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,63 +1,63 @@
-// if(JSON.parse(localStorage.getItem("fundraiser_list"))){
+if(JSON.parse(localStorage.getItem("fundraiser_list"))){
 
-// }
-// else{
-//     let fundraiser_list = [
+}
+else{
+    let fundraiser_list = [
 
-//         {
-//             "raiser_user_id": 234,
+        {
+            "raiser_user_id": 234,
 
-//             "emerging_player_id": 0,
+            "emerging_player_id": 0,
 
-//             "minimum_amount": "2000",
-//             "selected_option": "myself",
-//             "selected_option1": "tenth",
-//             "Employment_status": "student",
-//             "sports_type": "cricket",
-//              "city_name": "Erode",
-//             "player_name": "Kohli", "player_age": "22",
-//             "player_image_url": "https://iili.io/HvSRHRS.jpg",
-//             "player_title": "Funding for cricket",
-//             "days_left": "2023-04-30",
-//             "certificate_arr": []
-//         },
-//         {
-//             "emerging_player_id": 1,
-//             "raiser_user_id": 234,
-//             "minimum_amount": "2000",
-//             "selected_option": "myself",
-//             "selected_option1": "tenth",
-//             "Employment_status": "student",
-//             "sports_type": "cricket",
-//              "city_name": "Erode",
-//             "player_name": "Kohli",
-//              "player_age": "22",
-//             "player_image_url": "https://iili.io/HvSRHRS.jpg",
-//             "player_title": "Funding for cricket",
-//             "days_left": "2023-04-30",
-//             "certificate_arr": []
-//         },
+            "minimum_amount": "2000",
+            "selected_option": "myself",
+            "selected_option1": "tenth",
+            "Employment_status": "student",
+            "sports_type": "cricket",
+             "city_name": "Erode",
+            "player_name": "Kohli", "player_age": "22",
+            "player_image_url": "https://iili.io/HvSRHRS.jpg",
+            "player_title": "Funding for cricket",
+            "days_left": "2023-04-30",
+            "certificate_arr": []
+        },
+        {
+            "emerging_player_id": 1,
+            "raiser_user_id": 234,
+            "minimum_amount": "2000",
+            "selected_option": "myself",
+            "selected_option1": "tenth",
+            "Employment_status": "student",
+            "sports_type": "cricket",
+             "city_name": "Erode",
+            "player_name": "Kohli",
+             "player_age": "22",
+            "player_image_url": "https://iili.io/HvSRHRS.jpg",
+            "player_title": "Funding for cricket",
+            "days_left": "2023-04-30",
+            "certificate_arr": []
+        },
         
-//         {
-//             "emerging_player_id": 2,
-//             "raiser_user_id": 235,
-//             "minimum_amount": "2000",
-//             "selected_option": "myself",
-//             "selected_option1": "tenth",
-//             "Employment_status": "student",
-//             "sports_type": "tfcghvjnk",
-//              "city_name": "Erode",
-//             "player_name": "Kohli",
-//              "player_age": "22",
-//             "player_image_url": "https://iili.io/HvSRHRS.jpg",
-//             "player_title": "Funding for cricket",
-//             "days_left": "2023-04-30",
-//             "certificate_arr": []
-//         },
+        {
+            "emerging_player_id": 2,
+            "raiser_user_id": 235,
+            "minimum_amount": "2000",
+            "selected_option": "myself",
+            "selected_option1": "tenth",
+            "Employment_status": "student",
+            "sports_type": "tfcghvjnk",
+             "city_name": "Erode",
+            "player_name": "Kohli",
+             "player_age": "22",
+            "player_image_url": "https://iili.io/HvSRHRS.jpg",
+            "player_title": "Funding for cricket",
+            "days_left": "2023-04-30",
+            "certificate_arr": []
+        },
     
     
-//     ]
+    ]
 
-//     localStorage.setItem("fundraiser_list", JSON.stringify(fundraiser_list))
-// }
+    localStorage.setItem("fundraiser_list", JSON.stringify(fundraiser_list))
+}
 
